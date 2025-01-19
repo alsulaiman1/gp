e284309d8f728226e2910786ebe83fe9a8a08cc5
alsulaiman1 <alsulaiman33@gmail.com>
2025-01-19 21:56:33 +0300

    Update server.js
    fix the bug

--- server.js
+++ server.js
@@ -1,50 +1,50 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+const url = require('url');
 
 const server = http.createServer((req, res) => {
-    let filePath = path.join(__dirname, req.url === '/' ? 'home.html' : req.url);
-    const extname = path.extname(filePath);
-    let contentType = 'text/html';
+    // Parse the request URL
+    const parsedUrl = url.parse(req.url, true); // `true` parses the query string into an object
+    const pathname = parsedUrl.pathname; // Get the path without the query string
 
+    // Resolve file path
+    let filePath = path.join(__dirname, pathname === '/' ? 'home.html' : pathname);
+    const extname = path.extname(filePath);
+
+    // Default content type
+    let contentType = 'text/html';
     switch (extname) {
-        case '.js':
-            contentType = 'text/javascript';
-            break;
-        case '.css':
-            contentType = 'text/css';
-            break;
-        case '.json':
-            contentType = 'application/json';
-            break;
-        case '.png':
-            contentType = 'image/png';
-            break;
-        case '.jpg':
-            contentType = 'image/jpg';
-            break;
-        case '.gif':
-            contentType = 'image/gif';
-            break;
-        case '.svg':
-            contentType = 'image/svg+xml';
-            break;
-        default:
-            contentType = 'text/html';
+        case '.js': contentType = 'text/javascript'; break;
+        case '.css': contentType = 'text/css'; break;
+        case '.json': contentType = 'application/json'; break;
+        case '.png': contentType = 'image/png'; break;
+        case '.jpg': contentType = 'image/jpg'; break;
+        case '.gif': contentType = 'image/gif'; break;
+        case '.svg': contentType = 'image/svg+xml'; break;
     }
 
-    fs.readFile(filePath, (error, content) => {
-        if (error) {
-            if (error.code == 'ENOENT') {
+    // Check if the file exists
+    fs.access(filePath, fs.constants.F_OK, (err) => {
+        if (err) {
+            if (err.code === 'ENOENT') {
                 res.writeHead(404, { 'Content-Type': 'text/html' });
                 res.end('<h1>404 Not Found</h1>', 'utf-8');
             } else {
                 res.writeHead(500);
-                res.end('Sorry, there was an error: ' + error.code + ' ..\n');
+                res.end(`Server Error: ${err.code}`, 'utf-8');
             }
         } else {
-            res.writeHead(200, { 'Content-Type': contentType });
-            res.end(content, 'utf-8');
+            // Read and serve the file
+            fs.readFile(filePath, (error, content) => {
+                if (error) {
+                    res.writeHead(500);
+                    res.end(`Error reading file: ${error.code}`, 'utf-8');
+                } else {
+                    res.writeHead(200, { 'Content-Type': contentType });
+                    res.end(content, 'utf-8');
+                }
+            });
         }
     });
 });
